f8e7cbf4f838fd680f16fb00086e85b71157ab9d
hh19xx <hookhull@gmail.com>
2025-11-25 05:48:19 +0900

    update: 分割

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     devToolbar: {
         enabled: false,
-    }
+    },
+    // GitHub Pages 上でのサイトの「絶対 URL」
+    // site: 'https://haruh19xx.github.io',
+    // プロジェクトページ用に、リポジトリ名を base に付ける
+    // base: '/astro-haruh19xx',
 });
